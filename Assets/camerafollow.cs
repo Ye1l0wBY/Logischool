84655becf727deee099ed9f868b95bdd87722c00
Ye1l0wBY <salut@gmail.com>
2025-11-15 12:45:24 +0100

    init

---
+++ Assets/camerafollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, Player.transform.position.x, 0.05f),
+            Mathf.Lerp(transform.position.y, Player.transform.position.y, 0.05f),
+            -1
+            );
+    }
+}
